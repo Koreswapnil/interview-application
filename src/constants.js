d0cbd8c0a0b4cdb613f39c576f3aca622a967701
Swapnil Kore <koreswapnil72@gmail.com>
2025-11-18 23:44:56 +0530

    all ok

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = '';
+export const BASE_URL = 'https://quiz-backend-nlvw.onrender.com';
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const QUIZ_URL = '/api/quiz';
